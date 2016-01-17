cbabac927a75d0cc4151bc9e7005cf26bc31909c
fm-labs <flowmotionlabs@gmail.com>
2016-01-17 20:51:50 +0100

    removed auto config loading from bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,10 +1,6 @@
 <?php
 use Cake\Core\Configure;
-/**
- * Automatically load app's upload configuration
- *
- * Copy upload.default.php to your app's config folder,
- * rename to upload.php and adjust contents
- */
-//@TODO Fallback to default upload configuration
-Configure::load('upload');
+
+if (!Configure::read('Upload')) {
+    die("Upload Plugin not configured");
+}
