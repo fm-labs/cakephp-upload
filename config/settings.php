01ba2f07e55cbbfc63b5447b0d2d5baf45a0fcff
fm-labs <flowmotionlabs@gmail.com>
2023-03-07 19:03:24 +0100

    i18n

--- config/settings.php
+++ config/settings.php
@@ -4,7 +4,7 @@ return [
         'Upload' => [
             'groups' => [
                 'Uploader.Default' => [
-                    'label' => __('Default uploader settings'),
+                    'label' => __d('upload', 'Default uploader settings'),
                 ],
             ],
 
@@ -12,59 +12,59 @@ return [
                 'Upload.enabled' => [
                     'group' => 'Uploader.Default',
                     'type' => 'boolean',
-                    'label' => __('Enable uploads'),
-                    'help' => __('Enables file upload system-wide (Only activate, if your application needs it)'),
+                    'label' => __d('upload', 'Enable uploads'),
+                    'help' => __d('upload', 'Enables file upload system-wide (Only activate, if your application needs it)'),
                     'default' => false,
                 ],
                 'Upload.minFileSize' => [
                     'group' => 'Uploader.Default',
                     'type' => 'string',
-                    'help' => __('Minimum file size in bytes'),
+                    'help' => __d('upload', 'Minimum file size in bytes'),
                     'default' => 1,
                 ],
                 'Upload.maxFileSize' => [
                     'group' => 'Uploader.Default',
                     'type' => 'string',
-                    'help' => __('Maximum file size in bytes'),
+                    'help' => __d('upload', 'Maximum file size in bytes'),
                     'default' => 2 * 1024 * 1024, // 2MB
                 ],
                 'Upload.mimeTypes' => [
                     'group' => 'Uploader.Default',
                     'type' => 'string',
-                    'help' => __('Maximum file size in bytes'),
+                    'help' => __d('upload', 'Maximum file size in bytes'),
                     'default' => 'image/*',
                 ],
                 'Upload.fileSlug' => [
                     'group' => 'Uploader.Default',
                     'type' => 'string',
-                    'help' => __('Replacement character for file names'),
+                    'help' => __d('upload', 'Replacement character for file names'),
                     'default' => '_',
                 ],
                 'Upload.fileExtensions' => [
                     'group' => 'Uploader.Default',
                     'type' => 'string',
-                    'help' => __('Maximum file size in bytes'),
+                    'help' => __d('upload', 'Maximum file size in bytes'),
                     'default' => 'jpg,jpeg,png,gif',
                 ],
                 'Upload.hashFilename' => [
                     'group' => 'Uploader.Default',
                     'type' => 'boolean',
-                    'label' => __('Hash filename'),
-                    'help' => __('Sets the filename to the hashed value of the original filename'),
+                    'label' => __d('upload', 'Hash filename'),
+                    'help' => __d('upload', 'Sets the filename to the hashed value of the original filename'),
                     'default' => false,
                 ],
                 'Upload.uniqueFilename' => [
                     'group' => 'Uploader.Default',
                     'type' => 'boolean',
-                    'label' => __('Unique filename'),
-                    'help' => __('Ensures each file is stored with a unique filename (to prevent accidental overwriting of files)'),
+                    'label' => __d('upload', 'Unique filename'),
+                    'help' => __d('upload', 'Ensures each file is stored with a unique filename (to prevent accidental overwriting of files)'),
                     'default' => false,
                 ],
                 'Upload.overwrite' => [
                     'group' => 'Uploader.Default',
                     'type' => 'boolean',
-                    'label' => __('Allow overwrite'),
-                    'help' => __('Allow overwritting of existent files'),
+                    'label' => __d('upload', 'Allow overwrite'),
+                    'help' => __d('upload', 'Allow overwritting of existent files'),
                     'default' => false,
                 ],
             ],
